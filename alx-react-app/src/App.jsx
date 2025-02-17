176b5f3fc6ea0b12bc68783e33624dbf7793f15c
matiyoskidane <matiyoskidane24@gmail.com>
2025-02-17 09:55:03 -0500

    new file

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,23 +1,16 @@
-import WelcomeMessage from './components/WelcomeMessage';
-
-function App() {
-    return (
-        <div>
-            <WelcomeMessage />
-        </div>
-    );
-}
-
-export default App;
+import WelcomeMessage from "./components/WelcomeMessage";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
+import UserProfile from "./components/UserProfile"; // Import UserProfile
 
 function App() {
     return (
         <div>
             <Header />
             <MainContent />
+            <WelcomeMessage />
+            <UserProfile name="Alice" age="25" bio="Loves hiking and photography" /> 
             <Footer />
         </div>
     );
