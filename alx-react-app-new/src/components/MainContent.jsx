b1e3a9077c8734c126b3419e121717cdd73ff8e5
matiyoskidane <matiyoskidane24@gmail.com>
2025-02-27 13:04:40 -0500

    new file

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,10 @@
+function MainContent() {
+    return (
+      <main style={{ padding: '20px', backgroundColor: '#f5f5f5', textAlign: 'center' }}>
+        <p>Welcome to the best city guide!</p>
+      </main>
+    );
+  }
+  
+  export default MainContent;
+  
