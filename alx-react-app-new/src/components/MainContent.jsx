c5a4bf70e747fe3d5b43b21320223309a7c464cd
matiyoskidane <matiyoskidane24@gmail.com>
2025-02-27 13:17:05 -0500

    edit

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,7 +1,9 @@
 function MainContent() {
     return (
       <main style={{ padding: '20px', backgroundColor: '#f5f5f5', textAlign: 'center' }}>
-        <p>Welcome to the best city guide!</p>
+        <p style={{ fontSize: '18px', color: 'black' }}>
+        I love to visit New York, Paris, and Tokyo.
+      </p>
       </main>
     );
   }
