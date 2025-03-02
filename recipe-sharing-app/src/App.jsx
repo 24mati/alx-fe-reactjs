be03dc35ca1404913e3878c09936450d27587103
matiyoskidane <matiyoskidane24@gmail.com>
2025-03-02 04:24:45 -0500

    new

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import routing
 import SearchBar from './components/SearchBar';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
+import RecipeDetails from './components/RecipeDetails'; // Import RecipeDetails
 
 const App = () => {
   return (
-    <div>
-      <h1>Recipe Sharing App</h1>
-      <SearchBar />
-      <AddRecipeForm />
-      <RecipeList />
-    </div>
+    <Router>
+      <div>
+        <h1>Recipe Sharing App</h1>
+        <SearchBar />
+        <AddRecipeForm />
+        <Routes>
+          <Route path="/" element={<RecipeList />} /> {/* Home Route */}
+          <Route path="/recipe/:recipeId" element={<RecipeDetails />} /> {/* Recipe Details Route */}
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
