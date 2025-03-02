6b52e4f6d8690e2d6e27a799ee10fd3dde68ce08
matiyoskidane <matiyoskidane24@gmail.com>
2025-03-02 12:37:06 -0500

    vgg

--- my-company/src/Components/Navbar.jsx
+++ my-company/src/Components/Navbar.jsx
@@ -1,13 +1,14 @@
-
 import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav style={{ padding: '10px', background: '#333', color: '#fff' }}>
-      <Link to="/" style={{ margin: '10px', color: '#fff', textDecoration: 'none' }}>Home</Link>
-      <Link to="/about" style={{ margin: '10px', color: '#fff', textDecoration: 'none' }}>About</Link>
-      <Link to="/services" style={{ margin: '10px', color: '#fff', textDecoration: 'none' }}>Services</Link>
-      <Link to="/contact" style={{ margin: '10px', color: '#fff', textDecoration: 'none' }}>Contact</Link>
+    <nav style={{ padding: "10px", background: "#333", color: "#fff" }}>
+      <ul style={{ display: "flex", listStyle: "none", gap: "15px" }}>
+        <li><Link to="/" style={{ color: "white", textDecoration: "none" }}>Home</Link></li>
+        <li><Link to="/about" style={{ color: "white", textDecoration: "none" }}>About</Link></li>
+        <li><Link to="/services" style={{ color: "white", textDecoration: "none" }}>Services</Link></li>
+        <li><Link to="/contact" style={{ color: "white", textDecoration: "none" }}>Contact</Link></li>
+      </ul>
     </nav>
   );
 }
