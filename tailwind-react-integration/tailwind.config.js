f1ced8e66241ab696f0bdb4d09375c48fa7e0331
matiyoskidane <matiyoskidane24@gmail.com>
2025-03-15 05:55:54 -0400

    ff

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,11 +1,11 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} 
 export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+}*/
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
